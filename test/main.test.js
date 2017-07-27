116baf9673dfab4c187ae0d5d957fe6fa4051636
Cyril Ologho <cyril.paul@andela.com>
2017-07-27 20:21:56 +0100

    Add test to check data is being saved and retrieved

--- test/main.test.js
+++ test/main.test.js
@@ -1,9 +1,46 @@
-import chai from 'chai';
-
 const expect = chai.expect;
+let store = new LocalStorageDB('documents');
 
-describe('Default test suite', () => {
-	it('should perform artithmetic computation', ()=>{
-		expect(2 + 3).to.equal(5);
+const users = [{
+		"name":"John Doe",
+		"email": "john@doe.com"
+	},
+	{
+		"name": "Jane Doe",
+		"email": "jane@doe.com"
+	}
+];
+
+
+describe('It should be able to create a datastore', () => {
+	it('should ensure a localStorage item was created after instantiation',() => {
+		expect(window.localStorage.getItem('documents')).to.be.a('string');
 	});
-}) 
+
+	it('should be able to save items', () => {
+		store.create('users', users);
+		store.create('users', { "name": "Mary Doe", "email": "may@doe.com" });
+		store.create('admin', {'name': 'admin', "email":'admin@system.com'});
+		store.create('admin', {'name': 'super-admin', "email":'super-admin@system.com'});
+		let items = JSON.parse(window.localStorage.getItem('documents'))['users'];
+		let adminItems = JSON.parse(window.localStorage.getItem('documents'))['admin'];
+
+		expect(items).to.have.lengthOf(3);
+		expect(items[0].name).to.equal(users[0].name);
+		expect(adminItems).to.have.lengthOf(2);
+
+	});
+
+
+	it('should be able to get specifc data', () => {
+		expect(store.get('users')).to.have.lengthOf(3);
+		expect(store.get('users', 1)).to.deep.equal(users[1]);
+		expect(store.get()).to.have.property('users');
+		expect(store.get()).to.have.property('admin');
+	});
+
+
+
+});
+
+
