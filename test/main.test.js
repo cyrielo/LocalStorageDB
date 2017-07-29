39fb5d0f59b99154ec41c7283777866a66845d36
Cyril Ologho <cyril.paul@andela.com>
2017-07-29 14:14:25 +0100

    refactor code and tests to pass eslint issues

--- test/main.test.js
+++ test/main.test.js
@@ -2,91 +2,86 @@ import chai from 'chai';
 import LocalStorageDB from '../src';
 
 const expect = chai.expect;
-let store = new LocalStorageDB('documents');
+const store = new LocalStorageDB('documents');
 
 const users = [{
-		"name":"John Doe",
-		"email": "john@doe.com"
-	},
-	{
-		"name": "Jane Doe",
-		"email": "jane@doe.com"
-	}
-];
+  name: 'John Doe',
+  email: 'john@doe.com'
+}, {
+  name: 'Jane Doe',
+  email: 'jane@doe.com'
+}];
 
 after(() => {
-	window.localStorage.clear();
+  window.localStorage.clear();
 });
 
 describe('Creating data', () => {
-	it('should ensure a localStorage item was created after instantiation',() => {
-		expect(window.localStorage.getItem('documents')).to.be.a('string');
-	});
+  it('should ensure a localStorage item was created after instantiation', () => {
+    expect(window.localStorage.getItem('documents')).to.be.a('string');
+  });
 
-	it('should be able to save an array of items at once', () => {
-		store.create('users', users);
-		let items = JSON.parse(window.localStorage.getItem('documents'))['users'];
-		expect(items).to.have.lengthOf(2);
-	});
+  it('should be able to save an array of items at once', () => {
+    store.create('users', users);
+    const items = JSON.parse(window.localStorage.getItem('documents')).users;
+    expect(items).to.have.lengthOf(2);
+  });
 
-	it('should be able to append a single object to existing collection', () => {
-		store.create('users', { "name": "Mary Doe", "email": "may@doe.com" });
-		let items = JSON.parse(window.localStorage.getItem('documents'))['users'];
-		expect(items).to.have.lengthOf(3);
-	});
+  it('should be able to append a single object to existing collection', () => {
+    store.create('users', { name: 'Mary Doe', email: 'may@doe.com' });
+    const items = JSON.parse(window.localStorage.getItem('documents')).users;
+    expect(items).to.have.lengthOf(3);
+  });
 
-	it('should generate a new collection for items with the same keys', () => {
-		store.create('admin', {'name': 'admin', "email":'admin@system.com'});
-		store.create('admin', {'name': 'super..admin', "email":'super.admin@system.com'});
-		let adminItems = JSON.parse(window.localStorage.getItem('documents'))['admin'];
-		expect(adminItems).to.be.an.instanceof(Array);
-		expect(adminItems).to.have.lengthOf(2);
-	});
-
+  it('should generate a new collection for items with the same keys', () => {
+    store.create('admin', { name: 'admin', email: 'admin@system.com' });
+    store.create('admin', { name: 'super..admin', email: 'super.admin@system.com' });
+    const adminItems = JSON.parse(window.localStorage.getItem('documents')).admin;
+    expect(adminItems).to.be.an.instanceof(Array);
+    expect(adminItems).to.have.lengthOf(2);
+  });
 });
 
 describe('Reading data', () => {
-	it('should be able to get specifc data', () => {
-		expect(store.get('users')).to.have.lengthOf(3);
-	});
+  it('should be able to get specifc data', () => {
+    expect(store.get('users')).to.have.lengthOf(3);
+  });
 
-	it('should be able to get specific item from a collection', () => {
-		expect(store.get('users', 1)).to.deep.equal(users[1]);
-	});
+  it('should be able to get specific item from a collection', () => {
+    expect(store.get('users', 1)).to.deep.equal(users[1]);
+  });
 
-	it('should be able to fetch all items in the store', () => {
-		expect(store.get()).to.have.property('users');
-		expect(store.get()).to.have.property('admin');
-	});
-
+  it('should be able to fetch all items in the store', () => {
+    expect(store.get()).to.have.property('users');
+    expect(store.get()).to.have.property('admin');
+  });
 });
 
 describe('Updating data', () => {
-	it('should be able to update specific data in a collection', () => {
-		let newAdminUser = {"name": "the-super-admin", "email": "super-admin@system.com"};
-		store.update(newAdminUser, "admin", 1)
-		expect(store.get('admin', 1)).to.deep.equal(newAdminUser);
-	});
+  it('should be able to update specific data in a collection', () => {
+    const newAdminUser = { name: 'the-super-admin', email: 'super-admin@system.com' };
+    store.update(newAdminUser, 'admin', 1);
+    expect(store.get('admin', 1)).to.deep.equal(newAdminUser);
+  });
 
-	it('should be able to overwrite data with new update', () => {
-		let newAdminUser = {"name": "the-super-admin", "email": "super-admin@system.com"};
-		store.update(newAdminUser, 'admin' );
-		expect(store.get('admin')).to.be.an('object');
-	});
+  it('should be able to overwrite data with new update', () => {
+    const newAdminUser = { name: 'the-super-admin', email: 'super-admin@system.com' };
+    store.update(newAdminUser, 'admin');
+    expect(store.get('admin')).to.be.an('object');
+  });
 });
 
 describe('Deleting data', () => {
-	it('should be able to delete an entire block', () => {
-		store.delete('admin');
-		expect(store.get('admin')).to.be.undefined;
-	});
+  it('should be able to remove an entire block', () => {
+    store.remove('admin');
+    expect(store.get('admin')).to.equal(undefined);
+  });
 
-	it('should be able to delete specific items from a collection', () => {
-		store.delete('users', 1);
-		expect(store.get('users')).to.have.lengthOf(2);
-		store.delete('users', 1);
-		expect(store.get('users')).to.have.lengthOf(1);
-	});
+  it('should be able to remove specific items from a collection', () => {
+    store.remove('users', 1);
+    expect(store.get('users')).to.have.lengthOf(2);
+    store.remove('users', 1);
+    expect(store.get('users')).to.have.lengthOf(1);
+  });
 });
-
 
